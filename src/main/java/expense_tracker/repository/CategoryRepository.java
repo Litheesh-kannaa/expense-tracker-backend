5e932e1be770d5c1f4af16217fc53856df69b538
Litheesh-kannaa <litheeshkannaa@gmail.com>
2026-06-16 02:05:09 +0530

    Added @Repository annotation

--- src/main/java/expense_tracker/repository/CategoryRepository.java
+++ src/main/java/expense_tracker/repository/CategoryRepository.java
@@ -2,7 +2,9 @@ package expense_tracker.repository;
 
 import expense_tracker.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository
         extends JpaRepository<Category, Long> {
 }
--- src/main/java/expense_tracker/repository/ExpenseRepository.java
+++ src/main/java/expense_tracker/repository/ExpenseRepository.java
@@ -2,9 +2,11 @@ package expense_tracker.repository;
 
 import expense_tracker.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ExpenseRepository
         extends JpaRepository<Expense, Long> {
 
